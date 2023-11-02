5a5c032349cd5f1724fa9d08f527ee9a239b6bcb
Dine5hk <dineshkd03.dk@gmail.com>
2023-11-02 18:38:25 +0530

    first commit

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -17,128 +17,98 @@ class Demo extends StatefulWidget {
 class _DemoState extends State<Demo> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Demo'),
-        actions: [
-          IconButton(onPressed: (){}, icon: Icon(Icons.search)),
-          IconButton(onPressed: (){}, icon: Icon(Icons.more_vert))
-        ],
-      ),
-      body: Container(
-      ),
-      drawer: Drawer(
-        child: Column(
-          children: [
-            UserAccountsDrawerHeader(
-              accountName: Text('Luffy'),
-              accountEmail: Text('luffy@gmail.com'),
-              currentAccountPicture: CircleAvatar(
-                backgroundImage: AssetImage('image/1.jpg'),
-              ),
-              otherAccountsPictures: [
-                CircleAvatar(
-                  backgroundImage: AssetImage('image/2.jpg'),
-                )
-              ],
-            ),
-            ListTile(
-              onTap: (){
-                Navigator.push(context, MaterialPageRoute(builder: (context) => Message()
-                )
-                );
-              },
-              leading: Icon(Icons.inbox),
-              title: Text('Inbox'),
-              trailing: Icon(Icons.arrow_forward_ios),
-            ),
-            ListTile(
-              onTap: (){
-                Navigator.push(context, MaterialPageRoute(builder: (context) => img()
-                )
-                );
-              },
-              leading: Icon(Icons.send),
-              title: Text('Sent'),
-              trailing: Icon(Icons.arrow_forward_ios),
-            ),
-            ListTile(
-              onTap: (){
-                Navigator.push(context, MaterialPageRoute(builder: (context) => Calc()
-                )
-                );
-              },
-              leading: Icon(Icons.drafts),
-              title: Text('Draft'),
-              trailing: Icon(Icons.arrow_forward_ios),
-            ),
-
-      ExpansionTile(
-        leading: Icon(Icons.book),
-        title: Text('Others'),
-        subtitle: Text('Choose'),
-        children: [
-            ListTile(
-              onTap: (){
-                Navigator.push(context, MaterialPageRoute(builder: (context) =>
-                    Message()));
-              },
-              leading: Icon(Icons.outbox),
-              title: Text('OutBox'),
-            ),
-          ListTile(
-            onTap: (){
-              Navigator.push(context, MaterialPageRoute(builder: (context) =>
-                  Message()));
-            },
-            leading: Icon(Icons.save),
-            title: Text('Save'),
-          ),
-          ListTile(
-            onTap: (){
-              Navigator.push(context, MaterialPageRoute(builder: (context) =>
-                  Message()));
-            },
-            leading: Icon(Icons.snooze),
-            title: Text('Snoozed'),
-          ),
-          ListTile(
-            onTap: (){
-              Navigator.push(context, MaterialPageRoute(builder: (context) =>
-                  Message()));
-            },
-            leading: Icon(Icons.star),
-            title: Text('Starred'),
-          ),
-          ],
-        ),
-      ],
-      ),
-      ),
-    );
-  }
-}
-
-class tab extends StatefulWidget {
-  const tab({super.key});
-
-  @override
-  State<tab> createState() => _tabState();
-}
-
-class _tabState extends State<tab> {
-  @override
-  Widget build(BuildContext context) {
     return DefaultTabController(
-      length: 5,
+      length: 3,
       child: Scaffold(
-        appBar: AppBar(
-          title: Text('WhatsApp'),
-          actions: [
-            IconButton(onPressed: (){}, icon: Icon(Icons.search)),
-            IconButton(onPressed: (){}, icon: Icon(Icons.more_vert))
-          ],
+        drawer: Drawer(
+          child: Column(
+            children: [
+              UserAccountsDrawerHeader(
+                accountName: Text('Luffy'),
+                accountEmail: Text('luffy@gmail.com'),
+                currentAccountPicture: CircleAvatar(
+                  backgroundImage: AssetImage('image/1.jpg'),
+                ),
+                otherAccountsPictures: [
+                  CircleAvatar(
+                    backgroundImage: AssetImage('image/2.jpg'),
+                  )
+                ],
+              ),
+              ListTile(
+                onTap: (){
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => Message()
+                  )
+                  );
+                },
+                leading: Icon(Icons.inbox),
+                title: Text('Inbox'),
+                trailing: Icon(Icons.arrow_forward_ios),
+              ),
+              ListTile(
+                onTap: (){
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => img()
+                  )
+                  );
+                },
+                leading: Icon(Icons.send),
+                title: Text('Sent'),
+                trailing: Icon(Icons.arrow_forward_ios),
+              ),
+              ListTile(
+                onTap: (){
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => Calc()
+                  )
+                  );
+                },
+                leading: Icon(Icons.drafts),
+                title: Text('Draft'),
+                trailing: Icon(Icons.arrow_forward_ios),
+              ),
 
+              ExpansionTile(
+                leading: Icon(Icons.book),
+                title: Text('Others'),
+                subtitle: Text('Choose'),
+                children: [
+                  ListTile(
+                    onTap: (){
+                      Navigator.push(context, MaterialPageRoute(builder: (context) =>
+                          Message()));
+                    },
+                    leading: Icon(Icons.outbox),
+                    title: Text('OutBox'),
+                  ),
+                  ListTile(
+                    onTap: (){
+                      Navigator.push(context, MaterialPageRoute(builder: (context) =>
+                          Message()));
+                    },
+                    leading: Icon(Icons.save),
+                    title: Text('Save'),
+                  ),
+                  ListTile(
+                    onTap: (){
+                      Navigator.push(context, MaterialPageRoute(builder: (context) =>
+                          Message()));
+                    },
+                    leading: Icon(Icons.snooze),
+                    title: Text('Snoozed'),
+                  ),
+                  ListTile(
+                    onTap: (){
+                      Navigator.push(context, MaterialPageRoute(builder: (context) =>
+                          Message()));
+                    },
+                    leading: Icon(Icons.star),
+                    title: Text('Starred'),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+        appBar: AppBar(
           bottom: TabBar(
             isScrollable: true,
             tabs: [
