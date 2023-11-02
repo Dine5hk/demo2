992601342233fb3ebacff6804fb7e9ce9aaf9b88
Dine5hk <dineshkd03.dk@gmail.com>
2023-11-02 14:18:49 +0530

    first commit

--- lib/Message.dart
+++ lib/Message.dart
@@ -5,9 +5,11 @@ void main() {
 }
 
 class Message extends StatelessWidget {
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           title: Text('Inbox'),
